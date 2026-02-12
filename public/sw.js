5591558644b7d5d1dea41f4942a8edc8ef373a68
mac <mac@192.168.1.9>
2026-02-12 01:39:58 +0100

    Deploy landing page via public directory

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,3 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('kasbah-v1').then(c => c.addAll([
-    '/', '/index.html', '/style.css', '/app.js', '/manifest.webmanifest', '/kasbah-logo.png'
-  ])));
-  self.skipWaiting();
-});
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', (e) => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request)));
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", () => {});
